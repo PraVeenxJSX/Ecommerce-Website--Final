7854dd3f46675de2f45c283d63ab4d100c7e3508
PraVeenxJSX <160173872+PraVeenxJSX@users.noreply.github.com>
2025-12-23 15:29:52 +0530

    email

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,6 +1,12 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ to, subject, html }) => {
+  console.log("📧 EMAIL CONFIG CHECK:");
+console.log("HOST:", process.env.EMAIL_HOST);
+console.log("PORT:", process.env.EMAIL_PORT);
+console.log("USER:", process.env.EMAIL_USER);
+console.log("PASS EXISTS:", !!process.env.EMAIL_PASS);
+
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
